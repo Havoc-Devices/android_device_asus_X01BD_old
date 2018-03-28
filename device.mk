a83f080f96d02fd979934b95d48d38e675162ee9
LuK1337 <priv.luk@gmail.com>
2018-03-28 14:58:11 +0200

    X01BD: Import light HAL
    Change-Id: I45705c37a512f43af969b7885bd701e1b7e88a17

--- device.mk
+++ device.mk
@@ -198,6 +198,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.x01bd
 
 # Media
 PRODUCT_COPY_FILES += \
