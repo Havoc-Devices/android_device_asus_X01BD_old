c49083b0d6bcddb433388c556c9e0d0308a06c02
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-07 01:22:59 +0200

    X01BD: import ZukPref package with Kcal support

--- device.mk
+++ device.mk
@@ -439,4 +439,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# ZukExtras
+PRODUCT_PACKAGES += \
+    ZukPref	
+
 $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
