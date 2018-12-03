b05f07a0bccd326dceea0129bc20ec8cb7c728ea
kubersharma001 <kubersharma001@gmail.com>
2018-12-03 22:13:47 +0530

    X01BD: import wifi configs
    Change-Id: I14b97f6c5e8d0587591a5552b8bb434409081353

--- device.mk
+++ device.mk
@@ -84,4 +84,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/fstman.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/fstman.ini \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
