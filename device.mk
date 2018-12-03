17a6f4d440279de5fe299348a56e47c156970bc9
kubersharma001 <kubersharma001@gmail.com>
2018-12-03 23:33:04 +0530

    X01BD: build ipacm/tetheroffload packages
    Change-Id: Id384320db2c7267b440743f952464e5130d474f1

--- device.mk
+++ device.mk
@@ -26,3 +26,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
