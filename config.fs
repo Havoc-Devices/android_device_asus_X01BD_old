53e3f4699ff3cb6ed76542285b121849eb1d3c9d
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 23:18:17 +0530

    X01BD: config.fs: cleanup
    Change-Id: I9c7eea1124b6d041074cd2e069202579bc1cd8a5

--- config.fs
+++ config.fs
@@ -1,20 +1,14 @@
+[AID_VENDOR_QCOM_DIAG]
+value:2950
+
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
-[AID_VENDOR_QDSS]
-value:2902
-
 [AID_VENDOR_RFS]
-value:2903
+value:2951
 
 [AID_VENDOR_RFS_SHARED]
-value:2904
-
-[AID_VENDOR_ADPL_ODL]
-value:2905
-
-[AID_VENDOR_QRTR]
-value:2906
+value:2952
 
 [vendor/bin/wcnss_filter]
 mode: 0755
@@ -110,13 +104,13 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+caps: WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+caps: WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
@@ -142,45 +136,15 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
-[vendor/bin/loc_launcher]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: SETUID SETGID
-
-[system/vendor/bin/loc_launcher]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: SETUID SETGID
-
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
-[vendor/bin/sensors.qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[firmware/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
