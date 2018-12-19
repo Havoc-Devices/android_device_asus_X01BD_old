7401e622c81ace57fd51e0f225fb51af3b780e70
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 22:01:28 +0530

    X01BD: build ebtables, ethertypes and libebtc package
    Change-Id: I5b0f8dcb1d41a7a2087e5c6c86d8d0c353622ca6

--- device.mk
+++ device.mk
@@ -64,6 +64,12 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# IPv6
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes \
+    libebtc
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
