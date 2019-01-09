5e44a9f7190dcc69cffba7ce5e32dfae74b6c34c
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-01-09 21:30:43 +0100

    X01BD: Build additional ril packages

--- device.mk
+++ device.mk
@@ -391,9 +391,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
     rild \
     librmnetctl \
     libxml2 \
