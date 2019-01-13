07b9891a39f86612b199170b4880033cc4662cde
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    X01BD: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -416,7 +416,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
-	
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
@@ -458,6 +458,10 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
