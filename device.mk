a9a2482f6fd44fa2db2b3c5f684d4129988f979d
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:26:04 +0530

    X01BD: define product density
    Change-Id: Ibebcd6f152e89a50dedaf97d272689d189c5c8b5

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
