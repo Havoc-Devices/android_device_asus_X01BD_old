f0ba0ef953d063dce02a8c0145dd1575c03810ef
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:07:31 +0530

    X01BD: build Health hals
    Change-Id: Ic30b350964ef93d247e0a8b7c3c01f1517227545

--- device.mk
+++ device.mk
@@ -141,6 +141,11 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
