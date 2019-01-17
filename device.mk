b53b4e40accf2804687c31df0ae26919fe830c5d
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:13:41 +0530

    X01BD: build thermal packages
    Change-Id: I15b5e6887984ce5e6a1e6dd84e5fe6a508fa96b2

--- device.mk
+++ device.mk
@@ -197,6 +197,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
