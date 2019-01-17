6ff02c9476ef486078f2eb9d2fdc3b22de361083
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 18:24:42 +0530

    X01BD: import and add IRQ configs
    Change-Id: I93f75b7f56ade1d0fbb6e104044fac36b95ec587

--- device.mk
+++ device.mk
@@ -98,6 +98,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
+
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
