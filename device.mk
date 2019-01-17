d7ff456d5d8a9fb3222029941e4287c3ca269ab6
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:55:56 +0530

    X01BD: import initial vendor props
    Change-Id: Ia37b4aea6acbc0ae531ed2597fcce5f0a75fdfc3

--- device.mk
+++ device.mk
@@ -394,6 +394,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
