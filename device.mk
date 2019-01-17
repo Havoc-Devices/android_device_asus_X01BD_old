5de7d28be47046b7cb2cc2f1578306ce63bc5bbb
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:48:57 +0530

    X01BD: build Media extensions packages
    Change-Id: Ia80959e679d5cf96b1fc2d40fdedc7d1b22a2064

--- device.mk
+++ device.mk
@@ -198,6 +198,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+    libavmediaserviceextensions \
+    libmediametrics \
+    libregistermsext \
+    mediametrics
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
