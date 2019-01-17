7259f0c6e8c5ac2c8db6495f60028527cbb5317b
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:08:14 +0530

    X01BD: build keymaster packages
    Change-Id: I84a28cbcd50976c3abe9e5b57ef0e1bff3ed3a1a

--- device.mk
+++ device.mk
@@ -174,6 +174,11 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-common \
