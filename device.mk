2cbd7369b62650ab6299ea5b60024dca74465295
Bauuuuu <abansode@outlook.com>
2019-01-21 18:10:08 +0100

    X01BD: build 32 bit frameworks.displayservice@1.0
    * needed for camera

--- device.mk
+++ device.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
