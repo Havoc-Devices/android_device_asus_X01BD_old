411d90d5389876cacf296e7a03c730fb1ec8687c
Bauuuuu <abansode@outlook.com>
2019-01-22 11:09:02 +0100

    X01BD: Add qti privileged app permissions

--- device.mk
+++ device.mk
@@ -395,6 +395,10 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
+#QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
