91fd9f1ed2238f8c22c71ec60a259ebff4628b63
Bauuuuu <abansode@outlook.com>
2019-01-26 21:47:45 +0100

    X01BD: FM: cleanup and update configuration

--- device.mk
+++ device.mk
@@ -356,7 +356,6 @@ PRODUCT_PACKAGES += \
     init.qcom.syspart_fixup.sh \
     init.qcom.usb.sh \
     init.qcom.wifi.sh \
-    init.qti.fm.sh \
     init.qti.ims.sh \
     init.qti.qseecomd.sh \
     qca6234-service.sh \
